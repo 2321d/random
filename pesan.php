3d9e18f27f3120f96d4b84c2548260131d57ef49
2321d <158499069+2321d@users.noreply.github.com>
2024-09-01 13:00:33 +0800

    Update pesan.php

--- pesan.php
+++ pesan.php
@@ -1,19 +1,12 @@
 <?php
-// Konfigurasi database
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "db_tiket"; // Ganti dengan nama database Anda
-
-// Buat koneksi
+$dbname = "db_tiket"; 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
-// Ambil data dari formulir
 $nama = $_POST['nama'];
 $email = $_POST['email'];
 $tanggal = $_POST['tanggal'];
@@ -21,19 +14,14 @@ $jumlah_peserta = $_POST['jumlah_peserta'];
 $jumlah_hari = $_POST['jumlah_hari'];
 $tujuan = $_POST['tujuan'];
 $paket_perjalanan = $_POST['paket_perjalanan'];
-
-//Harga Paket
 $harga_paket = array(
-    'standar' => 300000,
-    'premium' => 400000,
-    'VIP' => 500000
+    'standar' => 50000,
+    'premium' => 100000,
+    'VIP' => 150000
 );
 
-//total keseluruhan harga
 $total_harga = $jumlah_peserta * $jumlah_hari * $harga_paket[$paket_perjalanan];
 
-
-// Simpan data ke database
 $sql = "INSERT INTO tiket (nama, email, tanggal, jumlah_peserta, jumlah_hari, tujuan, paket_perjalanan, total_harga) 
         VALUES ('$nama', '$email', '$tanggal', '$jumlah_peserta', '$jumlah_hari', '$tujuan', '$paket_perjalanan', '$total_harga')";
 
@@ -42,6 +30,5 @@ if ($conn->query($sql) === TRUE) {
     } else {
     echo "<script>alert('Error: " . $conn->error . "');window.location.href='tampil.php';</script>";
 }
-// Tutup koneksi
 $conn->close();
 ?>
